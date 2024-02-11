d5af1e1f7259fcc4f767a72d1168fd22711b8b6f
Emmett <76142235+snakesford@users.noreply.github.com>
2024-02-11 12:55:25 -0800

    condensing

--- ChatBot.js
+++ ChatBot.js
@@ -19,25 +19,22 @@ class ChatBot extends EventEmitter {
 
     removeUser(name) {
         const index = this.users.indexOf(name);
-        if (index !== -1) {
-            this.users.splice(index, 1);
-            this.emit("leave", new Date(), name);
-            this.updateUsersList();
+        this.users.splice(index, 1);
+        this.emit("leave", new Date(), name);
+        this.updateUsersList();
 
-            const userListString = this.users.join(', ');
-            const goodbyeMessage = `Goodbye, ${name}! Currently in chat: ${userListString}`;
-            this.emit("post", goodbyeMessage);
-        }
+        const userListString = this.users.join(', ');
+        const goodbyeMessage = `Goodbye, ${name}! Currently in chat: ${userListString}`;
+        this.emit("post", goodbyeMessage);
     }
 
     updateUsersList() {
         console.log('Current users:', this.users);
     }
+
 }
 
 module.exports = ChatBot;
-//chat bot is going to react to events that the chat connector emitts
-// 6:58
 //@mentions chat bot
 //@chatbot time
 //@chatbot catch unknown names
